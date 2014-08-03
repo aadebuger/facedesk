a752d691acb68c3da9a643eab0228de3abc696eb
aadebuger <admin@local.host>
2014-08-03 14:34:54 +0800

    first commit

---
+++ js/listr.js
@@ -0,0 +1,19 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: aadebuger
+ * Date: 14-7-29
+ * Time: 下午1:28
+ * To change this template use File | Settings | File Templates.
+ */
+
+ (function($)
+  {
+
+      $(document).ready(function(){
+
+          alert("hello120");
+
+
+      });
+
+})(jQuery);
