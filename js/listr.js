da5ff79d53c923bbf78fb67b8fbc5fe1aead7f06
aadebuger <admin@local.host>
2014-08-03 14:55:13 +0800

    first commit

--- js/listr.js
+++ js/listr.js
@@ -17,6 +17,10 @@
 
               alert("JSON Data: " + json);
               alert( json.length);
+              for ( i=0;i< json.length;i ++)
+              {
+$("#tableid").append("<li class='table-view-cell media'><a class='navigate-right'><img class='media-object pull-left' src='/uploads/0.jpg' height='96'> <div class='media-body'>  1 </div></a>   </li>'");
+              }
           });
 
 
